f51f381a22675d7311912da435ded2158a6f20ea
Frael4 <frael.o1805@gmail.com>
2025-01-21 23:47:51 -0500

    cambios

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-"# sistema-academico" 
+# Sistema Academico
+
+Proyecto final de la materia Gestión de la Configuración del Software
+
+## Pantalla principal
+
+[Pantalla princial](/images/pantalla-principal.png)
+
+### Menu de estudiantes
+
+[Menú de estudiantes](/images/menu-estudiantes.png)
+
+### Menu de cursos
+
+[Menú de cursos](/images/menu-cursos.png)
--- src/test/java/com/ug/test_unitarios/EstudianteTest.java
+++ src/test/java/com/ug/test_unitarios/EstudianteTest.java
@@ -8,6 +8,12 @@ public class EstudianteTest {
 
     @Test
     public void testAgregarEstudiante() {
-        Estudiante estudiante = new Estudiante(1, "Juan", "Perez", "0956986020", "jose@email.com", "0939262243", "Olmedo", "04/07/1998", "Activo", "01/01/2021");
+        Estudiante estudiante = new Estudiante(1, "Juan", "Perez", "0956986020", "jose@email.com", "0939262243",
+                "Olmedo", "04/07/1998", "Activo", "01/01/2021", 1);
+        assert estudiante != null;
+        assert estudiante.getIdEstudiante() == 1;
+        assert estudiante.getNombre().equals("Juan");
+        assert estudiante.getApellido().equals("Perez");
+        assert estudiante.getCedula().equals("0956986020");
     }
 }

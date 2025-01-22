18ab0251e83f55c841c8a007fad016c5781988d3
Frael4 <frael.o1805@gmail.com>
2025-01-21 23:41:32 -0500

    Rediseño de la ventana principal

--- src/main/java/com/ug/SistemaAcademico.java
+++ src/main/java/com/ug/SistemaAcademico.java
@@ -1,63 +1,105 @@
 package com.ug;
 
-import javax.swing.JOptionPane;
+import java.awt.AlphaComposite;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 import com.ug.view.AsistenciaView;
+import com.ug.view.CalificacionView;
+import com.ug.view.CursoView;
 import com.ug.view.EstudianteView;
 
 /**
  * Sistema Academico
  *
  */
-public class SistemaAcademico {
+public class SistemaAcademico extends JFrame {
+
+    final ImageIcon FONDO = new ImageIcon(getClass().getResource("/images/sistema-academico.png"));
+    final JLabel lblFondo = new JLabel(FONDO);
+
+    /*
+     * Main
+     */
     public static void main(String[] args) {
         SistemaAcademico sistemaAcademico = new SistemaAcademico();
-        sistemaAcademico.menuInicio();
+        sistemaAcademico.setVisible(true);
     }
 
-    public void menuInicio() {
-
-        String opciones[] = { ("Estudiantes"),
-                ("Cursos"),
-                ("Calificaciones"),
-                ("Asistencias"),
-                ("Salir")
-        };
-
-        int opcion = JOptionPane.showOptionDialog(
-                null,
-                "Seleccione un menú:",
-                "Sistema Academico",
-                JOptionPane.DEFAULT_OPTION,
-                JOptionPane.PLAIN_MESSAGE,
-                null,
-                opciones,
-                opciones[0]);
-
-        // int opcion = scanner.nextInt();
-
-        switch (opcion) {
-            case 0:
-                EstudianteView estudianteView = new EstudianteView();
-                estudianteView.setVisible(true);
-                break;
-            case 1:
-                // eliminarContacto();
-                break;
-            case 2:
-                // buscarContacto();
-                break;
-            case 3:
-                AsistenciaView asistenciaView = new AsistenciaView();
-                asistenciaView.setVisible(true);;
-                break;
-            case 4:
-                System.out.println("Saliendo del menu ...");
-                
-                break;
-
-            default:
-                break;
+    // Panel principal
+    JPanel panel = new JPanel() {
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            // Establecer la opacidad para todo el panel
+            Graphics2D g2d = (Graphics2D) g;
+            AlphaComposite alpha = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.7f); // 70% de opacidad
+            g2d.setComposite(alpha);
+            g2d.fillRect(0, 0, getWidth(), getHeight()); // Rellenar con el fondo semi-transparente
         }
+    };
+
+    JButton btnEstudiantes = new JButton("Estudiantes");
+    JButton btnCursos = new JButton("Cursos");
+    JButton btnCalificaciones = new JButton("Calificaciones");
+    JButton btnAsistencias = new JButton("Asistencias");
+
+    SistemaAcademico() {
+        setTitle("Sistema Academico");
+        setSize(500, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setLayout(null);
+
+        panel.setLayout(null);
+        panel.setBounds(0, 0, getWidth(), getHeight());
+        add(panel);
+        panel.setOpaque(false);
+
+        // Fondo
+        lblFondo.setBounds(0, 0, getWidth(), getHeight());
+        add(lblFondo);
+
+        panel.add(btnEstudiantes);
+        btnEstudiantes.setBounds(50, 50, 150, 30);
+        panel.add(btnCursos);
+        btnCursos.setBounds(50, 100, 150, 30);
+        panel.add(btnCalificaciones);
+        btnCalificaciones.setBounds(50, 150, 150, 30);
+        panel.add(btnAsistencias);
+        btnAsistencias.setBounds(50, 200, 150, 30);
+
+        // Eventos
+        btnEstudiantes.addActionListener((e) -> {
+            this.dispose();
+            EstudianteView estudianteView = new EstudianteView();
+            estudianteView.setVisible(true);
+        });
+
+        btnCursos.addActionListener((e) -> {
+            this.dispose();
+            CursoView cursoView = new CursoView();
+            cursoView.setVisible(true);
+        });
+
+        btnCalificaciones.addActionListener((e) -> {
+            this.dispose();
+            CalificacionView calificacionView = new CalificacionView();
+            calificacionView.setVisible(true);
+        });
+
+        btnAsistencias.addActionListener((e) -> {
+            this.dispose();
+            AsistenciaView asistenciaView = new AsistenciaView();
+            asistenciaView.setVisible(true);
+        });
 
     }
 
